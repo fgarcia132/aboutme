bf322ad831e230b33086dfac9e111cd1ceb995bd
Francisco Garcia <ciscog11@msn.com>
2016-01-13 13:41:14 -0700

    changed wording on line 11

--- public_html/me/me.php
+++ public_html/me/me.php
@@ -12,11 +12,11 @@
 
 		<p>In my free time I like to play Tennis and Game on the PC<br>
 			Here are some of the games I've been playing recently</p>
-		<img src="http://cdn.akamai.steamstatic.com/steam/apps/292030/header.jpg?t=1450694670"</a>
-		<img src="http://cdn.akamai.steamstatic.com/steam/apps/107410/header.jpg?t=1450888194"</a>
+		<img src="http://cdn.akamai.steamstatic.com/steam/apps/292030/header.jpg?t=1450694670"
+		<img src="http://cdn.akamai.steamstatic.com/steam/apps/107410/header.jpg?t=1450888194"
 		<div>
-			<img src="http://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg?t=1452221296"</a>
-			<img src="http://cdn.akamai.steamstatic.com/steam/apps/244210/header.jpg?t=1450200427"</a>
+			<img src="http://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg?t=1452221296"
+			<img src="http://cdn.akamai.steamstatic.com/steam/apps/244210/header.jpg?t=1450200427"
 				  </div>
 
 	</body>
