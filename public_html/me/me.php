f953bb8c6337f4a6dd177a05115ebab75e1e7ac2
Francisco Garcia <ciscog11@msn.com>
2016-01-13 11:33:07 -0700

    added head title and body

---
+++ public_html/me/me.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Francisco Garcia</title>
+	</head>
+	<body>
+		<h1>Me</h1>
+		<p>My name is Francisco but I also go by Cisco.</p>
+		<p>I'm from Seattle</p>
+	</body>
+</html>
