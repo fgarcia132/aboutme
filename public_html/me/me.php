2fec50d052143eb9eea288bf1a429f655b5e4537
Francisco Garcia <ciscog11@msn.com>
2016-01-14 08:10:30 -0700

    added to stylesheet

--- public_html/me/me.php
+++ public_html/me/me.php
@@ -6,6 +6,7 @@
 		<link href='https://fonts.googleapis.com/css?family=Chango' rel='stylesheet' type='text/css'>
 		<link href="../css/stylesheet.css" rel="stylesheet" type="text/css">
 
+
 	</head>
 	<body>
 		<div class="bg-aqua" id="p1">
@@ -17,11 +18,11 @@
 			<p>In my free time I like to play Tennis and Game on the PC<br>
 			Here are some of the games I've been playing recently</p>
 			<br/>
-			<img src="http://cdn.akamai.steamstatic.com/steam/apps/292030/header.jpg?t=1450694670"</a>
-			<img src="http://cdn.akamai.steamstatic.com/steam/apps/107410/header.jpg?t=1450888194"</a>
+			<img src="http://cdn.akamai.steamstatic.com/steam/apps/292030/header.jpg?t=1450694670"
+			<img src="http://cdn.akamai.steamstatic.com/steam/apps/107410/header.jpg?t=1450888194"
 			<br>
-			<img src="http://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg?t=1452221296"</a>
-			<img src="http://cdn.akamai.steamstatic.com/steam/apps/244210/header.jpg?t=1450200427"</a>
+			<img src="http://cdn.akamai.steamstatic.com/steam/apps/730/header.jpg?t=1452221296"
+			<img src="http://cdn.akamai.steamstatic.com/steam/apps/244210/header.jpg?t=1450200427"
 				  </div>
 		<br/>
 		<p>Thanks for looking!</p>
